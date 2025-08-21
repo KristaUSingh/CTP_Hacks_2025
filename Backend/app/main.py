b64de44a7c4ca3cc49723aa900b23179b46bc2af
Krista U. Singh <109554138+KristaUSingh@users.noreply.github.com>
2025-08-21 14:49:53 -0400

    Update main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import majors, plan
 
+
 app = FastAPI(title="Course Planner API")
+
+@app.get("/")
+def root():
+    return {"status": "Backend running on Vercel 🚀"}
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,3 +22,7 @@ app.include_router(plan.router, prefix="/generate-plan", tags=["plan"])
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
